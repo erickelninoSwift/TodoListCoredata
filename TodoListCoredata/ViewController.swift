a4596b7c55a4167a5903812295e466ff586c7627
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-06-18 13:16:13 +0200

    ToDolist

--- TodoListCoredata/ViewController.swift
+++ TodoListCoredata/ViewController.swift
@@ -8,13 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var erickArrays:[String] = ["Find IOS tutorials","Download all videos","Learn Objective-C"]
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "tololistcoredata") as? [String]
+        {
+            erickArrays = items
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return erickArrays.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = erickArrays[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(erickArrays[indexPath.row])
+        
+//        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        }else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
-
+    
+    @IBAction func AddButton(_ sender: UIBarButtonItem)
+    {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        
+        let Action = UIAlertAction(title: "Add item", style: UIAlertAction.Style.default) { (action) in
+            ////
+           self.erickArrays.append(textfield.text!)
+            self.defaults.set(self.erickArrays, forKey: "tololistcoredata")
+            
+            self.tableView.reloadData()
+        }
+        
+            alert.addTextField { (alerttextField) in
+            alerttextField.placeholder = "Create new Item"
+//            print(alerttextField.text!)
+                textfield = alerttextField
+                
+        }
+        
+        alert.addAction(Action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
